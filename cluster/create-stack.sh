6c59555a29b4bab1db225a6c918a11b4594eaacf
Sandor Szuecs <sandor.szuecs@zalando.de>
2016-10-21 12:08:49 +0200

    deploy latest mate and use dynamic hosted_zone to the the right dns zone

--- cluster/create-stack.sh
+++ cluster/create-stack.sh
@@ -15,6 +15,6 @@ key_arn=$(aws kms create-key --description "Kubernetes cluster $cluster $ver" | 
 aws kms create-alias --alias-name "alias/${cluster}-${ver}" --target-key-id "$key_arn"
 token=$(cat /dev/urandom | LC_ALL=C tr -dc _A-Z-a-z-0-9- | head -c 64)
 # TODO: encrypt fixed token with KMS
-userdata_master=$(cat userdata-master.yaml|sed -e s/STACK_VERSION/$ver/ -e s/ETCD_DISCOVERY_DOMAIN/$etcd_discovery_domain/ -e s,API_SERVER,$api_server, -e s/WORKER_SHARED_SECRET/$token/|gzip|base64 -w 0)
-userdata_worker=$(cat userdata-worker.yaml|sed -e s/STACK_VERSION/$ver/ -e s/ETCD_DISCOVERY_DOMAIN/$etcd_discovery_domain/ -e s,API_SERVER,$api_server, -e s/WORKER_SHARED_SECRET/$token/|gzip|base64 -w 0)
+userdata_master=$(cat userdata-master.yaml|sed -e s/STACK_VERSION/$ver/ -e s/ETCD_DISCOVERY_DOMAIN/$etcd_discovery_domain/ -e s,API_SERVER,$api_server, -e s/WORKER_SHARED_SECRET/$token/ -e s/HOSTED_ZONE/$hosted_zone/ |gzip|base64 )
+userdata_worker=$(cat userdata-worker.yaml|sed -e s/STACK_VERSION/$ver/ -e s/ETCD_DISCOVERY_DOMAIN/$etcd_discovery_domain/ -e s,API_SERVER,$api_server, -e s/WORKER_SHARED_SECRET/$token/|gzip|base64 )
 senza create senza-definition.yaml $ver UserDataMaster="$userdata_master" UserDataWorker="$userdata_worker" KmsKey="$key_arn"
