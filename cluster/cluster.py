5cff6805c900c962d6104d27ac11705b5887c144
Henning Jacobs <henning@jacobs1.de>
2016-10-24 11:57:05 +0200

    #15 use correct cluster name

--- cluster/cluster.py
+++ cluster/cluster.py
@@ -7,6 +7,9 @@ import gzip
 import random
 import subprocess
 import string
+import yaml
+
+from clickclick import info
 
 
 def get_user_data(fn, variables: dict) -> str:
@@ -18,20 +21,35 @@ def get_user_data(fn, variables: dict) -> str:
     return b64_encoded.decode('ascii')
 
 
-def get_cluster_variables(cluster_name: str, version: str):
+def get_account_alias_without_namespace():
+    '''
+    Return AWS account alias without an optional prefix (separated by dash)
+
+    i.e. an alias of "myorg-myteam" will return "myteam"
+    '''
+    iam = boto3.client('iam')
+    account_alias = iam.list_account_aliases()['AccountAliases'][0]
+    return account_alias.split('-')[-1]
+
+
+def get_cluster_variables(stack_name: str, version: str):
     route53 = boto3.client('route53')
     all_hosted_zones = route53.list_hosted_zones()['HostedZones']
     hosted_zone = all_hosted_zones[0]['Name'].rstrip('.')
+    account_alias_without_namespace = get_account_alias_without_namespace()
+
     etcd_discovery_domain = 'etcd.{}'.format(hosted_zone)
-    api_server = 'https://{}-{}.{}'.format(cluster_name, version, hosted_zone)
+    api_server = 'https://{}-{}.{}'.format(stack_name, version, hosted_zone)
     token = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(64))
+    cluster_name = '{}-{}'.format(account_alias_without_namespace, stack_name)
     # TODO: encrypt fixed token with KMS
     variables = {
         'stack_version': version,
         'etcd_discovery_domain': etcd_discovery_domain,
         'api_server': api_server,
         'worker_shared_secret': token,
-        'hosted_zone': hosted_zone
+        'hosted_zone': hosted_zone,
+        'webhook_cluster_name': cluster_name
     }
     return variables
 
@@ -42,37 +60,43 @@ def cli():
 
 
 @cli.command()
-@click.argument('cluster_name')
+@click.argument('stack_name')
 @click.argument('version')
-def create(cluster_name, version):
+@click.option('--dry-run', is_flag=True, help='No-op mode: show what would be created')
+def create(stack_name, version, dry_run):
     '''
     Create a new Kubernetes cluster (using current AWS credentials)
     '''
 
-    variables = get_cluster_variables(cluster_name, version)
+    variables = get_cluster_variables(stack_name, version)
+    info('Cluster name is:             {}'.format(variables['webhook_cluster_name']))
+    info('API server endpoint will be: {}'.format(variables['api_server']))
+    if dry_run:
+        print(yaml.safe_dump(variables))
     userdata_master = get_user_data('userdata-master.yaml', variables)
     userdata_worker = get_user_data('userdata-worker.yaml', variables)
-    subprocess.check_call(['senza', 'create', 'senza-definition.yaml', version, 'StackName={}'.format(cluster_name), 'UserDataMaster={}'.format(userdata_master), 'UserDataWorker={}'.format(userdata_worker), 'KmsKey=*'])
+    if not dry_run:
+        subprocess.check_call(['senza', 'create', 'senza-definition.yaml', version, 'StackName={}'.format(stack_name), 'UserDataMaster={}'.format(userdata_master), 'UserDataWorker={}'.format(userdata_worker), 'KmsKey=*'])
 
 
 @cli.command()
-@click.argument('cluster_name')
+@click.argument('stack_name')
 @click.argument('version')
-def update(cluster_name, version):
+def update(stack_name, version):
     '''
     Update Kubernetes cluster
     '''
-    variables = get_cluster_variables(cluster_name, version)
+    variables = get_cluster_variables(stack_name, version)
     userdata_master = get_user_data('userdata-master.yaml', variables)
     userdata_worker = get_user_data('userdata-worker.yaml', variables)
-    subprocess.check_call(['senza', 'update', 'senza-definition.yaml', version, 'StackName={}'.format(cluster_name), 'UserDataMaster={}'.format(userdata_master), 'UserDataWorker={}'.format(userdata_worker), 'KmsKey=*'])
+    subprocess.check_call(['senza', 'update', 'senza-definition.yaml', version, 'StackName={}'.format(stack_name), 'UserDataMaster={}'.format(userdata_master), 'UserDataWorker={}'.format(userdata_worker), 'KmsKey=*'])
 
 
 @cli.command()
-@click.argument('cluster_name')
+@click.argument('stack_name')
 @click.argument('version')
-def delete(cluster_name, version):
-    subprocess.check_call(['senza', 'delete', cluster_name, version])
+def delete(stack_name, version):
+    subprocess.check_call(['senza', 'delete', stack_name, version])
 
 
 if __name__ == '__main__':
