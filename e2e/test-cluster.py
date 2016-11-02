de3d45efcf414e81f539f6afa46a3a7f06dc632e
Henning Jacobs <henning@jacobs1.de>
2016-11-02 13:27:27 +0100

    give containers more time

--- e2e/test-cluster.py
+++ e2e/test-cluster.py
@@ -36,7 +36,7 @@ def main(url, token):
     all_containers_ready = False
 
     with Action('Waiting for all containers to be ready..') as act:
-        for i in range(20):
+        for i in range(30):
             containers = get_containers(url, token)
             ready = True
             for name in EXPECTED_CONTAINERS:
