28442342f6b463b6ee8a3f8ed08c0f527513bda5
Sandor Szücs <sandor.szuecs@zalando.de>
2018-05-18 11:42:50 +0200

    add script params

--- dev-to-alpha.sh
+++ dev-to-alpha.sh
@@ -1,26 +1,40 @@
 #!/bin/sh
+
+if [ -z $2 ]
+then
+  printf "$0 <base> <head>
+
+  example dev-to-alpha:
+  base=dev head=alpha
+"
+ exit 1
+fi
+
+BASE=$1
+HEAD=$2
+
 set -e
 MSG=/tmp/pr-message
-git checkout dev
+git checkout ${BASE}
 git pull --rebase
-git checkout alpha
+git checkout ${HEAD}
 git pull --rebase
 set +e
-git branch -D dev-to-alpha
+git branch -D ${BASE}-to-${HEAD}
 set -e
-git checkout -b dev-to-alpha
-git merge dev
-git push origin dev-to-alpha
+git checkout -b ${BASE}-to-${HEAD}
+git merge ${BASE}
+git push origin ${BASE}-to-${HEAD}
 
-echo "dev-to-alpha\n"> $MSG
-git log --pretty=format:'%B' alpha..dev | egrep -iv '^$|^Merge |^Signed-off-by|^Dev to alpha' | uniq >> $MSG
+printf "${BASE}-to-${HEAD}\n"> $MSG
+git log --pretty=format:'%B' ${HEAD}..${BASE} | egrep -iv '^$|^Merge |^Signed-off-by|dev to alpha|alpha to beta' | uniq >> $MSG
 
 if [ -x $(which hub) ]
 then
-  hub pull-request -b alpha -h dev-to-alpha -F $MSG -l ready-to-test
+  hub pull-request -b ${HEAD} -h ${BASE}-to-${HEAD} -F $MSG -l ready-to-test
 else
   echo "============================
-create a PR with base alpha and head dev-to-alpha at https://github.com/zalando-incubator/kubernetes-on-aws
+create a PR with base ${HEAD} and head ${BASE}-to-${HEAD} at https://github.com/zalando-incubator/kubernetes-on-aws
 release notes:"
   cat $MSG
   echo "============================"
