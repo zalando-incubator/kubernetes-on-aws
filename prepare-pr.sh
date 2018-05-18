21eb98af788d7892bc124747ba9cc2db2a970b48
Sandor Szücs <sandor.szuecs@zalando.de>
2018-05-18 11:54:39 +0200

    use shellcheck to fix all warnings

--- prepare-pr.sh
+++ prepare-pr.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ -z $2 ]
+if [ -z "${2}" ]
 then
-  printf "$0 <base> <head>
+  printf "%s <base> <head>
 
   example dev-to-alpha:
   base=dev head=alpha
-"
+" "${0}"
  exit 1
 fi
 
@@ -15,23 +15,25 @@ HEAD=$2
 
 set -e
 MSG=/tmp/pr-message
-git checkout ${BASE}
+git checkout "${BASE}"
 git pull --rebase
-git checkout ${HEAD}
+git checkout "${HEAD}"
 git pull --rebase
 set +e
-git branch -D ${BASE}-to-${HEAD}
+git branch -D "${BASE}-to-${HEAD}"
 set -e
-git checkout -b ${BASE}-to-${HEAD}
-git merge ${BASE}
-git push origin ${BASE}-to-${HEAD}
+git checkout -b "${BASE}-to-${HEAD}"
+git merge "${BASE}"
+git push origin "${BASE}-to-${HEAD}"
 
-printf "${BASE}-to-${HEAD}\n"> $MSG
-git log --pretty=format:'%B' ${HEAD}..${BASE} | egrep -iv '^$|^Merge |^Signed-off-by|dev to alpha|alpha to beta' | uniq >> $MSG
+printf "%s-to-%s
 
-if [ -x $(which hub) ]
+" "${BASE}" "${HEAD}" > "${MSG}"
+git log --pretty=format:'%B' "${HEAD}".."${BASE}" | grep -Eiv '^$|^Merge |^Signed-off-by|dev to alpha|alpha to beta' | uniq >> "${MSG}"
+
+if [ -x "$(which hub)" ]
 then
-  hub pull-request -b ${HEAD} -h ${BASE}-to-${HEAD} -F $MSG -l ready-to-test
+  hub pull-request -b "${HEAD}" -h "${BASE}-to-${HEAD}" -F "${MSG}" -l ready-to-test
 else
   echo "============================
 create a PR with base ${HEAD} and head ${BASE}-to-${HEAD} at https://github.com/zalando-incubator/kubernetes-on-aws
