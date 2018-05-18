cd78e2cf01edfa8c0cf10f950e7d5912d8a0c511
Sandor Szücs <sandor.szuecs@zalando.de>
2018-05-18 11:00:26 +0200

    prepare PR message and title and create pull request please update your github.com/github/hub known issue: on create-pullrequest it opens your editor https://github.com/github/hub/issues/1741

--- alpha-to-beta.sh
+++ alpha-to-beta.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 set -e
+MSG=/tmp/pr-message
 git checkout alpha
 git pull --rebase
 git checkout beta
 git pull --rebase
+set +e
 git branch -D alpha-to-beta
+set -e
 git checkout -b alpha-to-beta
 git merge alpha
 git push origin alpha-to-beta
-echo "============================
-ADD release notes in the PR
-============================"
 
+
+echo "alpha-to-beta\n"> $MSG
+git log --pretty=format:'%B' beta..alpha | egrep -iv '^$|^Merge |^Signed-off-by|^Dev to alpha|^alpha to beta' | uniq >> $MSG
+
+if [ -x $(which hub) ]
+then
+  hub pull-request -b beta -h alpha-to-beta -F $MSG -l ready-to-test
+else
+  echo "============================
+create a PR with base beta and head alpha-to-beta at https://github.com/zalando-incubator/kubernetes-on-aws
+release notes:"
+  cat $MSG
+  echo "============================"
+fi
--- dev-to-alpha.sh
+++ dev-to-alpha.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
-cd ~/git/teapot/kubernetes-on-aws
 set -e
+MSG=/tmp/pr-message
 git checkout dev
 git pull --rebase
 git checkout alpha
 git pull --rebase
+set +e
 git branch -D dev-to-alpha
+set -e
 git checkout -b dev-to-alpha
 git merge dev
 git push origin dev-to-alpha
 
-msgBODY=$(git log --pretty=format:'%B' alpha..dev | egrep -v '^$|^Merge |^Signed-off-by |^Dev to alpha')
+echo "dev-to-alpha\n"> $MSG
+git log --pretty=format:'%B' alpha..dev | egrep -iv '^$|^Merge |^Signed-off-by|^Dev to alpha' | uniq >> $MSG
+
 if [ -x $(which hub) ]
 then
-  hub pull-request -m "dev-to-alpha\n\n${msgBODY}" -b alpha -h dev-to-alpha
-  #l ready-to-test
+  hub pull-request -b alpha -h dev-to-alpha -F $MSG -l ready-to-test
 else
   echo "============================
 create a PR with base alpha and head dev-to-alpha at https://github.com/zalando-incubator/kubernetes-on-aws
-ADD release notes in the PR:
-${msgBODY}
-============================"
+release notes:"
+  cat $MSG
+  echo "============================"
 fi
