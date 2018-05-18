31041f38883e26d4d6f3e670bc4ce811dae7bb4c
Sandor Szücs <sandor.szuecs@zalando.de>
2018-05-18 10:00:34 +0200

    add PR handling if hub is installed and prepare message

--- dev-to-alpha.sh
+++ dev-to-alpha.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd ~/git/teapot/kubernetes-on-aws
 set -e
 git checkout dev
 git pull --rebase
@@ -8,7 +9,16 @@ git branch -D dev-to-alpha
 git checkout -b dev-to-alpha
 git merge dev
 git push origin dev-to-alpha
-echo "============================
-ADD release notes in the PR
-============================"
 
+msgBODY=$(git log --pretty=format:'%B' alpha..dev | egrep -v '^$|^Merge |^Signed-off-by |^Dev to alpha')
+if [ -x $(which hub) ]
+then
+  hub pull-request -m "dev-to-alpha\n\n${msgBODY}" -b alpha -h dev-to-alpha
+  #l ready-to-test
+else
+  echo "============================
+create a PR with base alpha and head dev-to-alpha at https://github.com/zalando-incubator/kubernetes-on-aws
+ADD release notes in the PR:
+${msgBODY}
+============================"
+fi
