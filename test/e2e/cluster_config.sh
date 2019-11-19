1e383e50c49c14d4486d86fb33ee53cafbe088c5
Mikkel Oscar Lyderik Larsen <mikkeloscar@users.noreply.github.com>
2019-11-19 12:15:03 +0100

    Revert "Update kube-ingress-aws-controller to v0.9.0 (NLB support)"

--- test/e2e/cluster_config.sh
+++ test/e2e/cluster_config.sh
@@ -52,7 +52,6 @@ clusters:
     efs_id: ${EFS_ID}
     webhook_id: ${INFRASTRUCTURE_ACCOUNT}:${REGION}:kube-aws-test
     node_problem_detector_enabled: true
-    kube_aws_ingress_controller_nlb_enabled: "true"
   criticality_level: 1
   environment: e2e
   id: ${CLUSTER_ID}
