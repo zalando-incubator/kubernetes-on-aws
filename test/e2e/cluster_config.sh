05cb6f564619a352a1e9b6bab27e69a6e428ffb5
Martin Linkhorst <martin.linkhorst@zalando.de>
2020-03-25 10:08:15 +0100

    chore: get pull request ready for merging
    Signed-off-by: Martin Linkhorst <martin.linkhorst@zalando.de>

--- test/e2e/cluster_config.sh
+++ test/e2e/cluster_config.sh
@@ -57,7 +57,6 @@ clusters:
     vm_dirty_background_bytes: 67108864
     aws_pod_identity_webhook_key: ${POD_IDENTITY_WEBHOOK_KEY}
     aws_pod_identity_webhook_cert: ${POD_IDENTITY_WEBHOOK_CERT}
-    teapot_admission_controller_service_account_iam: true
     prometheus_tsdb_retention_size: enabled
   criticality_level: 1
   environment: e2e
