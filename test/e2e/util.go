6b3ad5e238f46d2d1429c24fae8728a208d0656a
Martin Linkhorst <martin.linkhorst@zalando.de>
2023-02-13 17:03:32 +0100

    use dedicated load test and plain vegeta images

--- test/e2e/util.go
+++ test/e2e/util.go
@@ -895,7 +895,7 @@ func createVegetaDeployment(hostPath string, rate int) *appsv1.Deployment {
 					Containers: []corev1.Container{
 						{
 							Name:    name,
-							Image:   "container-registry.zalando.net/teapot/vegeta:v12.8.4-main-1",
+							Image:   "container-registry.zalando.net/teapot/vegeta:v12.8.4-main-2",
 							Command: []string{"sh", "-c"},
 							Args:    []string{cmd},
 							Resources: corev1.ResourceRequirements{
