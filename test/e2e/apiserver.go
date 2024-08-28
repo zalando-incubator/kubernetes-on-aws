aa5113ef0304235c87aa70f4704267891a085a29
Noor Malik <noor.malik@zalando.de>
2024-08-28 10:20:55 +0200

    use same registry for compliant and non-compliant images

--- test/e2e/apiserver.go
+++ test/e2e/apiserver.go
@@ -37,7 +37,8 @@ import (
 )
 
 const (
-	compliantImage1 = "container-registry.zalando.net/teapot/skipper:v0.21.0" // these are several compliant images
+	// these are several compliant images
+	compliantImage1 = "container-registry.zalando.net/teapot/skipper:v0.21.0"
 	compliantImage2 = "container-registry.zalando.net/teapot/skipper:v0.21.1"
 	compliantImage3 = "container-registry.zalando.net/teapot/skipper:v0.21.2"
 	compliantImage4 = "container-registry.zalando.net/teapot/skipper:v0.21.3"
@@ -47,16 +48,16 @@ const (
 	compliantImage8 = "container-registry.zalando.net/teapot/skipper:v0.21.7"
 
 	// these are non-compliant because of expired base image
-	nonCompliantImage1  = "registry.opensource.zalan.do/teapot/skipper:v0.14.0"
-	nonCompliantImage2  = "registry.opensource.zalan.do/teapot/skipper:v0.14.1"
-	nonCompliantImage3  = "registry.opensource.zalan.do/teapot/skipper:v0.14.2"
-	nonCompliantImage4  = "registry.opensource.zalan.do/teapot/skipper:v0.14.3"
-	nonCompliantImage5  = "registry.opensource.zalan.do/teapot/skipper:v0.14.4"
-	nonCompliantImage6  = "registry.opensource.zalan.do/teapot/skipper:v0.14.5"
-	nonCompliantImage7  = "registry.opensource.zalan.do/teapot/skipper:v0.14.6"
-	nonCompliantImage8  = "registry.opensource.zalan.do/teapot/skipper:v0.14.7"
-	nonCompliantImage9  = "registry.opensource.zalan.do/teapot/skipper:v0.14.8"
-	nonCompliantImage10 = "registry.opensource.zalan.do/teapot/skipper:v0.14.9"
+	nonCompliantImage1  = "container-registry.zalando.net/teapot/skipper:v0.19.0"
+	nonCompliantImage2  = "container-registry.zalando.net/teapot/skipper:v0.19.1"
+	nonCompliantImage3  = "container-registry.zalando.net/teapot/skipper:v0.19.2"
+	nonCompliantImage4  = "container-registry.zalando.net/teapot/skipper:v0.19.3"
+	nonCompliantImage5  = "container-registry.zalando.net/teapot/skipper:v0.19.4"
+	nonCompliantImage6  = "container-registry.zalando.net/teapot/skipper:v0.19.5"
+	nonCompliantImage7  = "container-registry.zalando.net/teapot/skipper:v0.19.6"
+	nonCompliantImage8  = "container-registry.zalando.net/teapot/skipper:v0.19.7"
+	nonCompliantImage9  = "container-registry.zalando.net/teapot/skipper:v0.19.8"
+	nonCompliantImage10 = "container-registry.zalando.net/teapot/skipper:v0.19.9"
 	waitForPodTimeout   = 5 * time.Minute
 )
 
