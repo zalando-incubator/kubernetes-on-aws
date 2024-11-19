df9c47b9e8d2838f50ccd5333bc9bfb9f090a626
Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>
2024-11-19 17:31:14 +0100

    Test e2e for EKS ipv6
    Signed-off-by: Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>

--- test/e2e/cluster_config.sh
+++ test/e2e/cluster_config.sh
@@ -53,6 +53,7 @@ clusters:
     skipper_open_policy_agent_bucket_arn: "${SKIPPER_OPA_BUCKET_ARN}"
     skipper_open_policy_agent_observability_url: "${SKIPPER_OPA_OBSERVABILITY_URL}"
     skipper_open_policy_agent_bundles_url: "${SKIPPER_OPA_BUNDLES_URL}"
+    eks_ip_family: "ipv6"
   criticality_level: 1
   environment: e2e
   id: ${CLUSTER_ID}
