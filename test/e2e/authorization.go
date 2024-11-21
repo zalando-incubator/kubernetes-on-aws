31825a135ab782f20b852d365f44abbeac1356e4
Noor Malik <noor.malik@zalando.de>
2024-11-21 22:55:42 +0100

    implement all the testcase specs

--- test/e2e/authorization.go
+++ test/e2e/authorization.go
@@ -244,76 +244,334 @@ var _ = g.Describe("Authorization [RBAC] [Zalando]", func() {
 	})
 
 	g.Context("For CollaboratorPowerUser, CollaboratorManual and CollaboratorEmergency groups", func() {
-		g.When("the resource is a Secret", func() {
-			g.It("should allow read access to visibility namespace", func() {})
-			g.It("should deny read access to kube-system namespace", func() {})
+		var tc testCase
+		g.BeforeEach(func() {
+			tc.data.groups = [][]string{
+				{"CollaboratorPowerUser", "PowerUser"},
+				{"CollaboratorManual", "Manual"},
+				{"CollaboratorEmergency", "Emergency"},
+			}
+			tc.data.users = []string{"test-user"}
 		})
 
-		g.It("should deny write access to Nodes", func() {})
-		g.It("should allow write access to DaemonSets", func() {})
-		g.It("should allow deletion of CRDs", func() {})
-		g.It("should deny deletion of kube-system or visibility namespaces", func() {})
+		g.When("the resource is a Secret", func() {
+			g.BeforeEach(func() {
+				tc.data.resources = []string{"secrets"}
+				tc.data.verbs = []string{"get", "list", "watch"}
+			})
+
+			g.It("should allow read access to visibility namespace", func() {
+				tc.data.namespaces = []string{"visibility"}
+				tc.run(context.TODO(), cs, true)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
+			g.It("should deny read access to kube-system namespace", func() {
+				tc.data.namespaces = []string{"kube-system"}
+				tc.run(context.TODO(), cs, false)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
+		})
+
+		g.It("should deny write access to Nodes", func() {
+			tc.data.resources = []string{"nodes"}
+			tc.data.verbs = []string{"create", "update", "delete", "patch"}
+			tc.run(context.TODO(), cs, false)
+			gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+		})
+		g.It("should allow write access to DaemonSets", func() {
+			tc.data.resources = []string{"apps/daemonsets"}
+			tc.data.verbs = []string{"create", "update", "delete", "patch"}
+			tc.run(context.TODO(), cs, true)
+			gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+		})
+		g.It("should allow deletion of CRDs", func() {
+			tc.data.resources = []string{"apiextensions.k8s.io/customresourcedefinitions"}
+			tc.data.verbs = []string{"delete"}
+			tc.run(context.TODO(), cs, true)
+			gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+		})
+		g.It("should deny deletion of kube-system or visibility namespaces", func() {
+			tc.data.resources = []string{"namespaces"}
+			tc.data.namespaces = []string{"kube-system", "visibility"}
+			tc.data.verbs = []string{"delete"}
+			tc.run(context.TODO(), cs, false)
+			gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+		})
 
 		g.When("the resource is a namespaced resource", func() {
-			g.It("should deny write access in kube-system namespace", func() {})
-			g.It("should allow write access in namespaces other than kube-system", func() {})
+			g.BeforeEach(func() {
+				tc.data.resources = []string{
+					"pods",
+					"apps/deployments",
+					"apps/statefulsets",
+					"services",
+					"persistentvolumes",
+					"persistentvolumeclaims",
+					"configmaps",
+				}
+				tc.data.verbs = []string{"create", "update", "delete", "patch"}
+			})
+			g.It("should deny write access in kube-system namespace", func() {
+				tc.data.namespaces = []string{"kube-system"}
+				tc.run(context.TODO(), cs, false)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
+			g.It("should allow write access in namespaces other than kube-system", func() {
+				tc.data.namespaces = []string{"", "teapot"}
+				tc.run(context.TODO(), cs, true)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
 		})
 
 		g.When("the resource is a global resource", func() {
-			g.It("should deny access to Nodes", func() {})
-			g.It("should allow access to resources other than Nodes", func() {})
+			g.BeforeEach(func() {
+				tc.data.verbs = []string{"create", "update", "delete", "patch"}
+			})
+			g.It("should deny access to Nodes", func() {
+				tc.data.resources = []string{"nodes"}
+				tc.run(context.TODO(), cs, false)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
+			g.It("should allow access to resources other than Nodes", func() {
+				tc.data.resources = []string{
+					"namespaces",
+					"storage.k8s.io/storageclasses",
+					"apiextensions.k8s.io/customresourcedefinitions",
+				}
+				tc.run(context.TODO(), cs, true)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
 		})
 	})
 
 	g.Context("For system users", func() {
+		var tc testCase
+
 		g.When("the user is kubelet", func() {
-			g.It("should allow to get Pods", func() {})
+			g.BeforeEach(func() {
+				tc.data.groups = [][]string{{"system:masters"}}
+				tc.data.users = []string{"kubelet"}
+			})
+			g.It("should allow to get Pods", func() {
+				tc.data.resources = []string{"pods"}
+				tc.data.verbs = []string{"get"}
+				tc.data.namespaces = []string{"teapot"}
+				tc.run(context.TODO(), cs, true)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
 		})
 
 		g.When("the service account is daemonset-controller", func() {
-			g.It("should allow to update DaemonSet status subresource", func() {})
-			g.It("should allow to update DaemonSet finalizers", func() {})
-			g.It("should allow to create Pods", func() {})
+			g.BeforeEach(func() {
+				tc.data.users = []string{"system:serviceaccount:kube-system:daemon-set-controller"}
+				tc.data.groups = [][]string{{"system:serviceaccounts:kube-system"}}
+			})
+			g.It("should allow to update DaemonSet status subresource", func() {
+				tc.data.resources = []string{"apps/daemonsets/status"}
+				tc.data.verbs = []string{"update"}
+				tc.run(context.TODO(), cs, true)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
+			g.It("should allow to update DaemonSet finalizers", func() {
+				tc.data.resources = []string{"apps/daemonsets/finalizers"}
+				tc.data.verbs = []string{"update"}
+				tc.run(context.TODO(), cs, true)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
+			// TODO: Need to verify in the original tests if this is a permission on
+			// the controller-manager or the daemonset-controller.
+			// g.It("should allow to create Pods", func() {})
 		})
 
-		g.When("the service account is default", func() {
-			g.It("should deny to list StatefulSets when in default namespace", func() {})
-			g.It("should deny to list StatefulSets when in non-default namespace", func() {})
+		g.When("the default service account is in default namespace", func() {
+			g.BeforeEach(func() {
+				tc.data.users = []string{"system:serviceaccount:default:default"}
+			})
+			g.It("should deny to list StatefulSets", func() {
+				tc.data.resources = []string{"apps/statefulsets"}
+				tc.data.verbs = []string{"list"}
+				tc.run(context.TODO(), cs, false)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
+		})
+
+		g.When("the default service account is in non-default namespace", func() {
+			g.BeforeEach(func() {
+				tc.data.users = []string{"system:serviceaccount:non-default:default"}
+			})
+			g.It("should deny to list StatefulSets", func() {
+				tc.data.resources = []string{"apps/statefulsets"}
+				tc.data.verbs = []string{"list"}
+				tc.run(context.TODO(), cs, false)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
 		})
 
 		g.When("the service account is persistent-volume-binder", func() {
-			g.It("should allow to update PersistentVolumeClaims", func() {})
-			g.It("should allow to create PersistentVolumes", func() {})
+			g.BeforeEach(func() {
+				tc.data.users = []string{"system:serviceaccount:kube-system:persistent-volume-binder"}
+				tc.data.groups = [][]string{{"system:serviceaccounts:kube-system"}}
+				tc.data.namespaces = []string{"kube-system"}
+			})
+			g.It("should allow to update PersistentVolumeClaims", func() {
+				tc.data.resources = []string{"persistentvolumeclaims"}
+				tc.data.verbs = []string{"update"}
+				tc.run(context.TODO(), cs, true)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
+			g.It("should allow to create PersistentVolumes", func() {
+				tc.data.resources = []string{"persistentvolumes"}
+				tc.data.verbs = []string{"create"}
+				tc.run(context.TODO(), cs, true)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
 
 		})
 
 		g.When("the service account is aws-cloud-provider", func() {
-			g.It("should allow to patch Nodes", func() {})
+			g.BeforeEach(func() {
+				tc.data.users = []string{"system:serviceaccount:kube-system:aws-cloud-provider"}
+				tc.data.groups = [][]string{{"system:serviceaccounts:kube-system"}}
+			})
+			g.It("should allow to patch Nodes", func() {
+				tc.data.resources = []string{"nodes"}
+				tc.data.verbs = []string{"patch"}
+				tc.run(context.TODO(), cs, true)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
 		})
 
 		g.When("the service account is api-monitoring-controller", func() {
-			g.It("should allow to update the skipper-default-filters ConfigMap in kube-system namespace", func() {})
-			g.It("should deny to update ConfigMaps other than skipper-default-filters", func() {})
+			g.BeforeEach(func() {
+				tc.data.users = []string{"system:serviceaccount:api-infrastructure:api-monitoring-controller"}
+			})
+			g.When("the namespace is kube-system", func() {
+				g.BeforeEach(func() {
+					tc.data.namespaces = []string{"kube-system"}
+				})
+				g.It("should allow to update 'skipper-default-filters' ConfigMap", func() {
+					tc.data.resources = []string{"configmaps"}
+					tc.data.verbs = []string{"update"}
+					tc.data.names = []string{"skipper-default-filters"}
+					tc.run(context.TODO(), cs, true)
+					gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+				})
+				g.It("should deny to update any other ConfigMap", func() {
+					tc.data.resources = []string{"configmaps"}
+					tc.data.verbs = []string{"update"}
+					// Technically, this should result in access undecided because we allow
+					// access to 'skipper-default-filters' ConfigMap only and we haven't
+					// specified a resource name in the test case.
+					// We consider access undecided cases also as denied.
+					tc.run(context.TODO(), cs, false)
+					gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+				})
+			})
 		})
 
 		g.When("the user is k8sapi_credentials-provider", func() {
-			g.It("should allow to get Secrets in kube-system namespace", func() {})
+			g.BeforeEach(func() {
+				tc.data.users = []string{"zalando-iam:zalando:service:k8sapi_credentials-provider"}
+			})
+			g.It("should allow to get Secrets in kube-system namespace", func() {
+				tc.data.resources = []string{"secrets"}
+				tc.data.namespaces = []string{"kube-system"}
+				tc.data.verbs = []string{"get"}
+				tc.run(context.TODO(), cs, true)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
 		})
 
 		g.When("the user is stups_cdp-controller", func() {
-			g.It("should deny access to Secrets in kube-system namespace", func() {})
+			g.BeforeEach(func() {
+				tc.data.users = []string{"zalando-iam:zalando:service:stups_cdp-controller"}
+			})
+			g.When("the namespace is kube-system", func() {
+				g.BeforeEach(func() {
+					tc.data.namespaces = []string{"kube-system"}
+				})
+				g.It("should deny to get Secrets", func() {
+					tc.data.resources = []string{"secrets"}
+					tc.data.verbs = []string{"get"}
+					tc.run(context.TODO(), cs, false)
+					gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+				})
+			})
 		})
 
 	})
 
 	g.Context("For administrators", func() {
-		g.It("should allow read access to resources other than Secrets in kube-system namespace", func() {})
-		g.It("should allow write access to resources other than Secrets in kube-system namespace", func() {})
-		g.It("should allow read access to Secrets in kube-system namespace", func() {})
-		g.It("should allow read access to Secrets in namespaces other than kube-system", func() {})
-		g.It("should allow write access to namespaces other than kube-system", func() {})
-		g.It("should allow to proxy", func() {})
-		g.It("should allow write access to DaemonSets", func() {})
-	})
+		var tc testCase
+		g.BeforeEach(func() {
+			tc.data.groups = [][]string{{"system:masters"}}
+			tc.data.users = []string{"nmalik"}
+		})
 
+		g.When("namespace is kube-system", func() {
+			g.BeforeEach(func() {
+				tc.data.namespaces = []string{"kube-system"}
+			})
+			g.When("the resource is a Secret", func() {
+				g.BeforeEach(func() {
+					tc.data.resources = []string{"secrets"}
+				})
+				g.It("should allow read access", func() {
+					tc.data.verbs = []string{"get", "list", "watch"}
+					tc.run(context.TODO(), cs, true)
+					gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+				})
+			})
+
+			g.When("the resource is not a Secret", func() {
+				g.BeforeEach(func() {
+					tc.data.resources = []string{"pods"}
+				})
+				g.It("should allow read access", func() {
+					tc.data.verbs = []string{"get", "list", "watch"}
+					tc.run(context.TODO(), cs, true)
+					gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+				})
+				g.It("should allow write access", func() {
+					tc.data.verbs = []string{"create", "update", "delete", "patch"}
+					tc.run(context.TODO(), cs, true)
+					gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+				})
+			})
+
+		})
+
+		g.When("namespace is not kube-system", func() {
+			g.BeforeEach(func() {
+				tc.data.namespaces = []string{"teapot"}
+			})
+
+			g.It("should allow to proxy", func() {
+				tc.data.verbs = []string{"proxy"}
+				tc.run(context.TODO(), cs, true)
+				gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+			})
+
+			g.When("the resource is a Secret", func() {
+				g.BeforeEach(func() {
+					tc.data.resources = []string{"secrets"}
+				})
+				g.It("should allow read access", func() {
+					tc.data.verbs = []string{"get", "list", "watch"}
+					tc.run(context.TODO(), cs, true)
+					gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+				})
+			})
+			g.When("the resource is not a Secret", func() {
+				g.BeforeEach(func() {
+					tc.data.resources = []string{"pods, apps/daemonsets"}
+				})
+				g.It("should allow write access", func() {
+					tc.data.verbs = []string{"create", "update", "delete", "patch"}
+					tc.run(context.TODO(), cs, true)
+					gomega.Expect(tc.output.passed).To(gomega.BeTrue(), tc.output.String())
+				})
+			})
+		})
+	})
 })
