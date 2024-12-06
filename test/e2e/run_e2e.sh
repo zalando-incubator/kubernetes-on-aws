be209f0b8151bc7fd37c2d9634ac48aa968e5790
nwickramasin <nuwandi.wickramasinghe@zalando.de>
2024-12-06 08:32:25 +0100

    Focus on ingress tests

--- test/e2e/run_e2e.sh
+++ test/e2e/run_e2e.sh
@@ -183,7 +183,7 @@ if [ "$e2e" = true ]; then
 
     mkdir -p junit_reports
     ginkgo -procs=25 -flake-attempts=2 \
-        -focus="(\[[Ingress]\])" \
+        -focus="(\[Ingress\])" \
         -skip="(\[Serial\]|validates.that.there.is.no.conflict.between.pods.with.same.hostPort.but.different.hostIP.and.protocol|Should.create.gradual.traffic.routes)" \
         "e2e.test" -- \
         -delete-namespace-on-failure=false \
