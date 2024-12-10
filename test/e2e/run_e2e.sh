65760708e8896e3dbdb1e31b84aab1964b0a8569
Noor Malik <noor.malik@zalando.de>
2024-12-10 10:53:18 +0100

    skip the existing RBAC e2e tests

--- test/e2e/run_e2e.sh
+++ test/e2e/run_e2e.sh
@@ -204,6 +204,7 @@ if [ "$e2e" = true ]; then
         SKIPPED_TESTS+=(
             "Mirror pods should be created for the main Kubernetes components \[Zalando\]"
             "Should audit API calls to create, update, patch, delete pods. \[Audit\] \[Zalando\]"
+            "should validate permissions for \[Authorization\] \[RBAC\] \[Zalando\]" # TODO: Remains skipped until we remove the older RBAC setup
         )
     fi
 
