60920c1f1a74b22e7caa1fd963861a00d79738f8
Katyanna Moura <amelie.kn@gmail.com>
2025-08-15 10:30:14 +0200

    Add flow example
    Signed-off-by: Katyanna Moura <amelie.kn@gmail.com>

--- test/e2e/authorization.go
+++ test/e2e/authorization.go
@@ -134,6 +134,10 @@ var _ = g.Describe("Authorization [RBAC] [Zalando]", func() {
     // Such requests bypass the admission controller, which only processes
     // mutating requests. Admission controller tests for this role are unnecessary,
     // as access control is fully enforced at the RBAC authorization stage.
+    // Flow example:
+    // 1. Request Received → RBAC checks role permissions.
+    // 2. Read-Only Role (`GET`) → Allowed by RBAC, **skips** admission controller.
+    // 3. Read-Only Role (`DELETE`) → Blocked by RBAC, so never reaches admission controller.
 	g.Context("For ReadOnly group", func() {
 		var tc testCase
 		g.BeforeEach(func() {
